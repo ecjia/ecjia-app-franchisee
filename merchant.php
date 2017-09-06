cf1c1a0b1ec25d6b8b5723f5d639ed4fd009c87e
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-09-06 11:53:23 +0800

    修改提示问题

--- merchant.php
+++ merchant.php
@@ -75,6 +75,7 @@ class merchant extends ecjia_merchant {
 	}
 
 	public function init() {
+		$this->unset_login_info();
 		$step 	= isset($_GET['step']) 		? $_GET['step'] 		: 1;
 		$type	= !empty($_GET['type']) 	? trim($_GET['type']) 	: '';
 		$mobile = !empty($_GET['mobile']) 	? trim($_GET['mobile']) : '';
@@ -207,7 +208,7 @@ class merchant extends ecjia_merchant {
 		$this->assign('form_action', RC_Uri::url('franchisee/merchant/insert', $arr));
 		
 		if (is_ie()) {
-			$browser_warning = '您当前的浏览器版本过低，建议升级您的浏览器或使用chrome内核浏览器！';
+			$browser_warning = '您当前的浏览器版本过低，建议升级您的浏览器或使用chrome内核浏览器！如：360极速浏览器、360安全浏览器（极速模式）、火狐浏览器、谷歌浏览器。';
 			$this->assign('browser_warning', $browser_warning);
 		}
 		$this->display('franchisee.dwt');
@@ -543,6 +544,7 @@ class merchant extends ecjia_merchant {
 	}
 	
 	public function view() {
+		$this->unset_login_info();
 		$this->assign('ur_here', '查询审核进度');
 		$this->assign('action_link', array('href' => RC_Uri::url('franchisee/merchant/init'), 'text' => '申请入驻'));
 		
@@ -742,6 +744,23 @@ class merchant extends ecjia_merchant {
 		return $this->db_region->where(array('region_id' => $id))->get_field('region_name');
 	}
 	
+	/**
+	 * 清除登录信息
+	 */
+	private function unset_login_info() {
+		if (isset($_SESSION['staff_id']) && intval($_SESSION['staff_id']) > 0) {
+			RC_Session::destroy();
+		}
+		$staff_id = RC_Cookie::get('ECJAP[staff_id]');
+		$staff_pass = RC_Cookie::get('ECJAP[staff_pass]');
+		if (!empty($staff_id) && !empty($staff_pass)) {
+			RC_Cookie::delete('ECJAP.staff_id');
+			RC_Cookie::delete('ECJAP.staff_pass');
+		}
+		unset($staff_id);
+		unset($staff_pass);
+		return;
+	}
 }
 
 // end
